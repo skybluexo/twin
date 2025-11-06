d47a5cfa77d9320bbde938059022ebc117d6a034
Francis Cho <fhcho1@yahoo.com>
2025-11-06 13:49:50 -0800

    Initial commit: Digital Twin infrastructure and application

---
+++ terraform/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Uses AWS CLI configuration (aws configure)
+}
+
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
